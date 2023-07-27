e819139cdc7248f9da088f93b155cc863bf2c0f9
Khanh <khanhpnv2@vng.com.vn>
2023-07-27 17:31:38 +0700

    Update dialogue - open pop up

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -11,89 +11,147 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text messageText;
     public RectTransform backgroundBox;
     public AudioSource messageSound;
+    public GameObject popup;
+    public GameObject closeButton; // Reference to the button to be closed
+    public float popupAppearDelay = 2f;
+    public float popupEaseDuration = 0.5f;
 
     Message[] currentMessages;
     Actor[] currentActors;
     int activeMessage = 0;
     public static bool isActive = false;
 
-    public void OpenDialogue(Message[] messages, Actor[] actors) {
+    public void OpenDialogue(Message[] messages, Actor[] actors)
+    {
         currentMessages = messages;
         currentActors = actors;
         activeMessage = 0;
         isActive = true;
         Debug.Log("Started convo! Loaded messages:" + messages.Length);
         DisplayMessage();
+
+        // Hide the closeButton if it exists
+        if (closeButton != null)
+        {
+            closeButton.SetActive(false);
+        }
+
         backgroundBox.LeanScale(Vector3.one, 0.5f).setEaseInOutExpo();
     }
 
-    void DisplayMessage() {
-        Message messageToDisplay = currentMessages[activeMessage];
-        messageText.text = messageToDisplay.message;
-
-        Actor actorToDisplay = currentActors[messageToDisplay.actorId];
-        actorName.text = actorToDisplay.name;
-        actorImage.sprite = actorToDisplay.sprite;
-
-        // Play the sound for the current actor
-        if (actorToDisplay.sound != null)
+    void DisplayMessage()
     {
-            AudioSource audioSource = GetComponent<AudioSource>();
-            audioSource.PlayOneShot(actorToDisplay.sound);
-            Debug.Log("Playing sound for actor: " + actorToDisplay.name);
-    }
-    else
-    {
-        Debug.Log("No sound assigned for actor: " + actorToDisplay.name);
-    }
-
-        AnimateTextColor();
-
-        // Play the message sound effect
-        if (messageSound != null && messageSound.clip != null)
+        if (activeMessage < currentMessages.Length)
         {
-            messageSound.Play();
-        }
+            Message messageToDisplay = currentMessages[activeMessage];
+            messageText.text = messageToDisplay.message;
 
+            Actor actorToDisplay = currentActors[messageToDisplay.actorId];
+            actorName.text = actorToDisplay.name;
+            actorImage.sprite = actorToDisplay.sprite;
+
+            // Play the sound for the current actor
+            if (actorToDisplay.sound != null)
+            {
+                AudioSource audioSource = GetComponent<AudioSource>();
+                audioSource.PlayOneShot(actorToDisplay.sound);
+                Debug.Log("Playing sound for actor: " + actorToDisplay.name);
+            }
+            else
+            {
+                Debug.Log("No sound assigned for actor: " + actorToDisplay.name);
+            }
+
+            AnimateTextColor();
+
+            // Play the message sound effect
+            if (messageSound != null && messageSound.clip != null)
+            {
+                messageSound.Play();
+            }
+        }
+        else
+        {
+            Debug.Log("Conversation ended!");
+            //backgroundBox.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+            isActive = false;
+
+            // Delay the popup appearance and add easing effect
+            StartCoroutine(ShowPopupWithDelay());
+        }
     }
 
-    public void NextMessage() {
+    public void NextMessage()
+    {
         activeMessage++;
-        if (activeMessage < currentMessages.Length) {
+        if (activeMessage < currentMessages.Length)
+        {
             DisplayMessage();
-        } else {
-            Debug.Log("Conversation ended!");
-            backgroundBox.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
-            isActive = false;
+        }
+        else
+        {
+            // End the dialogue
+            EndDialogue();
         }
     }
 
     void AnimateTextColor()
-{
+    {
         Color originalColor = messageText.color;
-         Color transparentColor = new Color(originalColor.r, originalColor.g, originalColor.b, 0f);
+        Color transparentColor = new Color(originalColor.r, originalColor.g, originalColor.b, 0f);
 
-         messageText.color = transparentColor;
-         LeanTween.value(gameObject, UpdateTextColor, transparentColor, originalColor, 0.5f)
-             .setOnUpdate((Color color) => messageText.color = color)
-             .setEase(LeanTweenType.easeInOutExpo);
-}
+        messageText.color = transparentColor;
+        LeanTween.value(gameObject, UpdateTextColor, transparentColor, originalColor, 0.5f)
+            .setOnUpdate((Color color) => messageText.color = color)
+            .setEase(LeanTweenType.easeInOutExpo);
+    }
 
     void UpdateTextColor(Color color)
-{
-         messageText.color = color;
-}
+    {
+        messageText.color = color;
+    }
+
+    void ShowPopup()
+    {
+        // Show the popup
+        popup.SetActive(true);
+    }
+
+    IEnumerator ShowPopupWithDelay()
+    {
+        // Wait for the specified delay before showing the popup
+        yield return new WaitForSeconds(popupAppearDelay);
+
+        // Show the popup with easing effect
+        ShowPopup();
+        popup.transform.localScale = Vector3.zero; // Start with zero scale
+        popup.LeanScale(Vector3.one, popupEaseDuration).setEase(LeanTweenType.easeInOutExpo);
+    }
+
+    void EndDialogue()
+    {
+        // Hide dialogue elements
+        actorImage.gameObject.SetActive(false);
+        actorName.gameObject.SetActive(false);
+        messageText.gameObject.SetActive(false);
+        backgroundBox.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+
+        // Show the popup
+        StartCoroutine(ShowPopupWithDelay());
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         backgroundBox.transform.localScale = Vector3.zero;
+        popup.SetActive(false); // Hide the popup at the start
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isActive == true) {
+        if (Input.GetKeyDown(KeyCode.Space) && isActive == true)
+        {
             NextMessage();
         }
     }
